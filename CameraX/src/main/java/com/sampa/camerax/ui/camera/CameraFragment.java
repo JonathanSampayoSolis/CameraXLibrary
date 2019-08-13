dc1029a02396afdd94119e513eaedf254ace7a4c
jjsampayo <jj.sampayo@mavi.mx>
2019-08-13 12:04:24 -0500

    Change camera bug fixed for moto G5

--- CameraX/src/main/java/com/sampa/camerax/ui/camera/CameraFragment.java
+++ CameraX/src/main/java/com/sampa/camerax/ui/camera/CameraFragment.java
@@ -15,8 +15,6 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.camera.core.CameraX;
-import androidx.camera.core.ImageAnalysis;
-import androidx.camera.core.ImageAnalysisConfig;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageCaptureConfig;
 import androidx.camera.core.Preview;
@@ -31,158 +29,158 @@ import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class CameraFragment extends Fragment implements PermissionsHelper.PermissionsHelperCallback {
-
-    private static final String[] PERMISSIONS = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
-
-    private final int PERMISSION_CODE = 1;
-
-    private FragmentCameraBinding binding;
-
-    private ImageCapture imageCapture;
-
-    private Preview preview;
-
-    private PermissionsHelper permissionsHelper;
-
-    private AtomicBoolean isLenBackActive;
-    
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        permissionsHelper = new PermissionsHelper(this, PERMISSION_CODE, PERMISSIONS, this);
-        isLenBackActive = new AtomicBoolean(true);
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return (binding = FragmentCameraBinding.inflate(inflater, container, false)).getRoot();
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        permissionsHelper.resolvePermissions();
-        
-        binding.fabFlash.setOnClickListener(v -> {
-            preview.enableTorch(!preview.isTorchOn());
-            binding.fabFlash.setImageResource(preview.isTorchOn() ?
-                    R.drawable.ic_flash_off_white : R.drawable.ic_flash_on_white);
-        });
+	
+	private static final String[] PERMISSIONS = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+	
+	private final int PERMISSION_CODE = 1;
+	
+	private FragmentCameraBinding binding;
+	
+	private ImageCapture imageCapture;
+	
+	private Preview preview;
+	
+	private PermissionsHelper permissionsHelper;
+	
+	private AtomicBoolean isLenBackActive;
+	
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		
-        binding.fabChangeCamera.setOnClickListener(v -> {
-            if (CameraX.isBound(preview)) CameraX.unbind(preview);
+		permissionsHelper = new PermissionsHelper(this, PERMISSION_CODE, PERMISSIONS, this);
+		isLenBackActive = new AtomicBoolean(true);
+	}
+	
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		return (binding = FragmentCameraBinding.inflate(inflater, container, false)).getRoot();
+	}
+	
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		permissionsHelper.resolvePermissions();
+		
+		binding.fabFlash.setOnClickListener(v -> {
+			preview.enableTorch(!preview.isTorchOn());
+			binding.fabFlash.setImageResource(preview.isTorchOn() ? R.drawable.ic_flash_off_white : R.drawable.ic_flash_on_white);
+		});
+		
+		binding.fabChangeCamera.setOnClickListener(v ->
+				bindCamera(isLenBackActive.getAndSet(!isLenBackActive.get()) ? CameraX.LensFacing.FRONT : CameraX.LensFacing.BACK)
+		);
+		
+	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		if (requestCode == PERMISSION_CODE)
+			permissionsHelper.resolvePermissions();
+	}
+	
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		if (requestCode == PERMISSION_CODE)
+			permissionsHelper.resolvePermissions();
+	}
+	
+	@Override
+	public void onPermissionsGranted() {
+		binding.textureView.post(this::startCamera);
+	}
+	
+	// region: REFERENCE METHODS
+	
+	private void startCamera() {
+		binding.textureView.addOnLayoutChangeListener((view, i, i1, i2, i3, i4, i5, i6, i7) -> updateTransform());
+		bindCamera(CameraX.LensFacing.BACK);
+	}
+	
+	// endregion
+	
+	// region:: PRIVATE METHODS
+	
+	private void bindCamera(CameraX.LensFacing lensFacing) {
+		CameraX.unbindAll();
+		CameraX.bindToLifecycle(getViewLifecycleOwner(), createPreview(lensFacing), createImageCapture(lensFacing));
+	}
+	
+	private Preview createPreview(CameraX.LensFacing lensFacing) {
+		DisplayMetrics displayMetrics = new DisplayMetrics();
+		Size size = new Size(displayMetrics.widthPixels, displayMetrics.heightPixels);
+		
+		Rational rational = new Rational(displayMetrics.widthPixels, displayMetrics.heightPixels);
+		
+		Preview preview = new Preview(
+				new PreviewConfig.Builder()
+						.setLensFacing(lensFacing)
+						.setTargetResolution(size)
+						.setTargetAspectRatio(rational)
+						.setTargetRotation(Objects.requireNonNull(getActivity()).getWindowManager().getDefaultDisplay().getRotation())
+						.build()
+		);
+		
+		preview.setOnPreviewOutputUpdateListener(output -> {
+			ViewGroup viewGroup = (ViewGroup) binding.textureView.getParent();
 			
-            CameraX.bindToLifecycle(getViewLifecycleOwner(), createPreview(isLenBackActive.getAndSet(!isLenBackActive.get()) ?
-                    CameraX.LensFacing.FRONT :
-                    CameraX.LensFacing.BACK));
-        });
-    
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == PERMISSION_CODE)
-            permissionsHelper.resolvePermissions();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == PERMISSION_CODE)
-            permissionsHelper.resolvePermissions();
-    }
-
-    @Override
-    public void onPermissionsGranted() {
-        binding.textureView.post(this::startCamera);
-    }
-
-    // region: REFERENCE METHODS
-
-    private void startCamera() {
-        binding.textureView.addOnLayoutChangeListener((view, i, i1, i2, i3, i4, i5, i6, i7) -> updateTransform());
-
-        CameraX.bindToLifecycle(getViewLifecycleOwner(), createPreview(CameraX.LensFacing.BACK), createImageAnalysis(), createImageCapture());
-    }
-
-    // endregion
-
-    // region:: PRIVATE METHODS
-
-    private Preview createPreview(CameraX.LensFacing lensFacing) {
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        Size size = new Size(displayMetrics.widthPixels, displayMetrics.heightPixels);
-
-        Rational rational = new Rational(displayMetrics.widthPixels, displayMetrics.heightPixels);
-
-        Preview preview = new Preview(
-                new PreviewConfig.Builder()
-                        .setLensFacing(lensFacing)
-                        .setTargetResolution(size)
-                        .setTargetAspectRatio(rational)
-                        .setTargetRotation(binding.textureView.getDisplay().getRotation())
-                        .build()
-        );
-
-        preview.setOnPreviewOutputUpdateListener(output -> {
-            ViewGroup viewGroup = (ViewGroup) binding.textureView.getParent();
-
-            viewGroup.removeView(binding.textureView);
-            viewGroup.addView(binding.textureView, 0);
-
-            binding.textureView.setSurfaceTexture(output.getSurfaceTexture());
-            updateTransform();
-        });
-
-        return this.preview = preview;
-    }
-
-    private ImageAnalysis createImageAnalysis() {
-        return new ImageAnalysis(
-                new ImageAnalysisConfig.Builder()
-                        .build()
-        );
-    }
-
-    private ImageCapture createImageCapture() {
-        return this.imageCapture = new ImageCapture(
-                new ImageCaptureConfig.Builder()
-                        .setTargetRotation(Objects.requireNonNull(getActivity()).getWindowManager().getDefaultDisplay().getRotation())
-                        .build()
-        );
-    }
-
-    private void updateTransform() {
-        Matrix matrix = new Matrix();
-
-        float centerX = binding.textureView.getWidth() / 2f;
-        float centerY = binding.textureView.getHeight() / 2f;
-
-        float rotationDegrees;
-        switch (binding.textureView.getDisplay().getRotation()) {
-            case Surface.ROTATION_0:
-                rotationDegrees = 0;
-                break;
-            case Surface.ROTATION_90:
-                rotationDegrees = 90;
-                break;
-            case Surface.ROTATION_180:
-                rotationDegrees = 180;
-                break;
-            case Surface.ROTATION_270:
-                rotationDegrees = 270;
-                break;
-            default:
-                return;
-        }
-
-        matrix.postRotate(-rotationDegrees, centerX, centerY);
-
-        binding.textureView.setTransform(matrix);
-    }
-
-    // endregion
-
+			viewGroup.removeView(binding.textureView);
+			viewGroup.addView(binding.textureView, 0);
+			
+			binding.textureView.setSurfaceTexture(output.getSurfaceTexture());
+			updateTransform();
+		});
+		
+		return this.preview = preview;
+	}
+	
+	private ImageCapture createImageCapture(CameraX.LensFacing lensFacing) {
+		DisplayMetrics displayMetrics = new DisplayMetrics();
+		Size size = new Size(displayMetrics.widthPixels, displayMetrics.heightPixels);
+		
+		Rational rational = new Rational(displayMetrics.widthPixels, displayMetrics.heightPixels);
+		
+		return this.imageCapture = new ImageCapture(
+				new ImageCaptureConfig.Builder()
+						.setLensFacing(lensFacing)
+						.setTargetResolution(size)
+						.setTargetAspectRatio(rational)
+						.setTargetRotation(Objects.requireNonNull(getActivity()).getWindowManager().getDefaultDisplay().getRotation())
+						.build()
+		);
+	}
+	
+	private void updateTransform() {
+		Matrix matrix = new Matrix();
+		
+		float centerX = binding.textureView.getWidth() / 2f;
+		float centerY = binding.textureView.getHeight() / 2f;
+		
+		float rotationDegrees;
+		switch (binding.textureView.getDisplay().getRotation()) {
+			case Surface.ROTATION_0:
+				rotationDegrees = 0;
+				break;
+			case Surface.ROTATION_90:
+				rotationDegrees = 90;
+				break;
+			case Surface.ROTATION_180:
+				rotationDegrees = 180;
+				break;
+			case Surface.ROTATION_270:
+				rotationDegrees = 270;
+				break;
+			default:
+				return;
+		}
+		
+		matrix.postRotate(-rotationDegrees, centerX, centerY);
+		
+		binding.textureView.setTransform(matrix);
+	}
+	
+	// endregion
+	
 }
